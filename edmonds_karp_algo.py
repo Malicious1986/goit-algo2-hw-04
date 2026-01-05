6c6968337a5891c6a4e1de945f950d5da3af02b6
Osadchyi Yurii <malicious1986@gmail.com>
2026-01-05 12:49:50 +0000

    done

---
+++ edmonds_karp_algo.py
@@ -0,0 +1,84 @@
+from collections import deque
+
+
+def bfs(capacity_matrix, flow_matrix, source, sink, parent):
+    visited = [False] * len(capacity_matrix)
+    queue = deque([source])
+    visited[source] = True
+
+    while queue:
+        current_node = queue.popleft()
+
+        for neighbor in range(len(capacity_matrix)):
+            if (
+                not visited[neighbor]
+                and capacity_matrix[current_node][neighbor]
+                - flow_matrix[current_node][neighbor]
+                > 0
+            ):
+                parent[neighbor] = current_node
+                visited[neighbor] = True
+                if neighbor == sink:
+                    return True
+                queue.append(neighbor)
+
+    return False
+
+
+def edmonds_karp(capacity_matrix, source, sink):
+    num_nodes = len(capacity_matrix)
+    flow_matrix = [[0] * num_nodes for _ in range(num_nodes)]
+    parent = [-1] * num_nodes
+    max_flow = 0
+
+    while bfs(capacity_matrix, flow_matrix, source, sink, parent):
+
+        path_flow = float("Inf")
+        current_node = sink
+
+        while current_node != source:
+            previous_node = parent[current_node]
+            path_flow = min(
+                path_flow,
+                capacity_matrix[previous_node][current_node]
+                - flow_matrix[previous_node][current_node],
+            )
+            current_node = previous_node
+
+        current_node = sink
+        while current_node != source:
+            previous_node = parent[current_node]
+            flow_matrix[previous_node][current_node] += path_flow
+            flow_matrix[current_node][previous_node] -= path_flow
+            current_node = previous_node
+
+        max_flow += path_flow
+
+    return max_flow
+
+
+# Матриця пропускної здатності для каналів у мережі
+# Індекси: 0-1: Термінали, 2-5: Склади, 6-19: Магазини
+capacity_matrix = [
+    # 0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19
+    [0, 0, 25, 20, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 0: Термінал 1
+    [0, 0, 0, 10, 15, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 1: Термінал 2
+    [0, 0, 0, 0, 0, 0, 15, 10, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 2: Склад 1
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 10, 25, 0, 0, 0, 0, 0, 0, 0, 0],  # 3: Склад 2
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 15, 10, 0, 0, 0, 0, 0],  # 4: Склад 3
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 10, 15, 5, 10],  # 5: Склад 4
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 6: Магазин 1
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 7: Магазин 2
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 8: Магазин 3
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 9: Магазин 4
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 10: Магазин 5
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 11: Магазин 6
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 12: Магазин 7
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 13: Магазин 8
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 14: Магазин 9
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 15: Магазин 10
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 16: Магазин 11
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 17: Магазин 12
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 18: Магазин 13
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 19: Магазин 14
+]
